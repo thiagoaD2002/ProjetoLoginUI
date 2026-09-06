6b3f5c6c7916803d50f0aa4c92cd56e27e0d2ac2
Thiago <100088838+thiagoaD2002@users.noreply.github.com>
2026-09-06 14:12:02 -0300

    att

--- src/js/pgHome.js
+++ src/js/pgHome.js
@@ -25,3 +25,15 @@ const a1 = new AnimaInput()
 
 export default AnimaInput
 
+class RemoveBtn{
+    constructor(){
+        const btnEntrar = document.querySelector(".btnEntrar")
+        btnEntrar.addEventListener("click",(evt)=>{
+            evt.preventDefault()
+
+            window.location.href = './pagLogou.html'
+        })
+    }
+}
+
+const r1 = new RemoveBtn()
